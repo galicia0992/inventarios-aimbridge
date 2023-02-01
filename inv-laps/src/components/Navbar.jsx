0512a2e27fe8dd341a47e9f0d772085221a0ca3e
Yair Galicia Muñiz <yair.galicia@aimbridge.com>
2023-02-01 17:02:56 -0600

    creacion menu burguer

--- inv-laps/src/components/Navbar.jsx
+++ inv-laps/src/components/Navbar.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import styled from "@emotion/styled"
+import {stack as Menu} from "react-burger-menu"
+import { NavLink } from 'react-router-dom'
+
+
+
+
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <>
+    <Menu width={100}>
+      <NavLink to={"/inventarios"}>Inventarios</NavLink>
+    </Menu>
+  
+    </>
   )
 }
 
