0b9a3185f9b670a91fa9e9907fcec387d86a15a9
Yair Galicia Muñiz <yair.galicia@aimbridge.com>
2023-02-02 17:54:46 -0600

    collapse y color sidebar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,39 +1,89 @@
 import React from 'react'
-import Hamburger from 'hamburger-react'
 import { useState } from 'react'
 import { Sidebar, Menu, MenuItem, SubMenu, useProSidebar } from 'react-pro-sidebar';
 import { NavLink } from 'react-router-dom';
 import styled from '@emotion/styled';
+import inventarioIcon from "../assets/inventory.png"
+import directorioIcon from "../assets/directory.png"
+
 const BotonCollapse = styled.button`
 background-color: white;
 border: none;
 cursor: pointer;
+&:hover{
+  background-color: aliceblue;
+}
 `
+const IconoInventario = styled.img`
+  width: 20px;
+`
+const WrapperInventario = styled.div`
+  display: flex;
+  flex-direction: row;
+`
+
 const Wrapper = styled.div`
   display: flex;
   flex-direction: row;
 `
 const Navbar = () => {
   const [expand, setExpand] = useState(false)
-  const [isOpen, setOpen] = useState(false)
   const { collapseSidebar } = useProSidebar();
   const expandCollapse = () => {
     expand == false ? setExpand(true):setExpand(false)
-    console.log(expand)
   }
+
+  const themes = {
+    light: {
+      sidebar: {
+        backgroundColor: '#ffffff',
+        color: '#607489',
+      },
+      menu: {
+        menuContent: '#fbfcfd',
+        icon: '#0098e5',
+        hover: {
+          backgroundColor: '#c5e4ff',
+          color: '#44596e',
+        },
+        disabled: {
+          color: '#9fb6cf',
+        },
+      },
+    },
+    dark: {
+      sidebar: {
+        backgroundColor: '#0b2948',
+        color: '#8ba1b7',
+      },
+      menu: {
+        menuContent: '#082440',
+        icon: '#59d0ff',
+        hover: {
+          backgroundColor: '#00458b',
+          color: '#b6c8d9',
+        },
+        disabled: {
+          color: '#3e5e7e',
+        },
+      },
+    },
+  };
   return (
     <>
-      <Wrapper style={{ display: "flex", height: "100%" }}>
-        <Sidebar>
-          <Menu>
-            <SubMenu label="Inventarios">
+      <Wrapper style={{ display: "flex", height: "100%"}} >
+        <Sidebar collapsedWidth="75px">
+          <Menu  rootStyles={{
+          backgroundColor:"#FFFF"
+        }}>
+            <SubMenu  label="Inventarios" icon={<IconoInventario src={inventarioIcon}></IconoInventario>}>
               <MenuItem component={<NavLink to={"/inventarios"}></NavLink>}>
                 {" "}
                 Inventarios{" "}
               </MenuItem>
               <MenuItem> Line charts </MenuItem>
             </SubMenu>
-            <SubMenu label="Directorio">
+            <SubMenu label="Directorio" icon={<IconoInventario src={directorioIcon}></IconoInventario>}>
               <MenuItem component={<NavLink to={"/"}></NavLink>}>WEMTYV</MenuItem>
             </SubMenu>
           </Menu>
